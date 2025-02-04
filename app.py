5a2303dec80b2d7a7c79f540f6eb79a1fff46159
gurupism003 <guruprasath22td0673@svcet.ac.in>
2025-02-04 14:39:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request 
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-         return "welcome to flask!"
-@app.route('/hello/<name>')
-def hello(name):
-         return f"hello, {name}!"
-if __name__ =='__main_':
-        app.run(debug=True)
+    return 'Hello, this is the home page'
+
+@app.route('/welcome/<name>')
+def welcome(name):
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
